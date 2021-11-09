2621d5adf0869ff958e3b46b4216d94efad95ce3
gizemnkorkmaz <gizemnurkorkmaz@gmail.com>
2021-11-09 12:26:17 +0300

    Add extras

---
+++ react-hooks/src/exercise/02-extra2.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') || initialName,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
